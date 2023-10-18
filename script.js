064f69d75394947ddbc78d8a2d8eb5e6d34107a9
juhilia <138122912+juhilia@users.noreply.github.com>
2023-10-18 07:37:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+botao.addEventListener ('click',function(){
+
+
+
+
+
+
+
+
+
+
+
+
+    var Nomecliente = nome.value;
+    var quantidadeSorvete = quantidade.value;
+    var saborSorvete = sabores.value;
+    
+    
+    
+    
+    
+    
+    
+    
+    /* alert( "Nome Cliente: " + NomeCliente + " Quantidade: " + quantidadeSorvete + " sabores: " + saborSorvete);*/
+    pedidoNome.innerHTML = "Nome:" + Nomecliente;
+    pedidoquantia.innerHTML = "Quantia:" + quantidadeSorvete;
+    pedidosabor.innerHTML = "Sabor:" + saborSorvete;
+    });
+    
+    
+    
+    
+    
+    
